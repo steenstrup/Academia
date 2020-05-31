df17cc11b30e81950eb4230485cdb8fe638119d6
kasper steenstrup <Kasper.steenstrup@gmail.com>
2020-05-31 21:30:06 +0200

    Add project files.

---
+++ Server/Controllers/ArticleController.cs
@@ -0,0 +1,36 @@
+﻿using Academia.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Academia.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArticleController : ControllerBase
+    {
+        private readonly ILogger<ArticleController> logger;
+
+        public ArticleController(ILogger<ArticleController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Article>>> Get()
+        {
+            List<Article> articles = new List<Article>();
+            using (FileStream fs = System.IO.File.OpenRead(@"C:\Git\blazor\Academia\Server\ressurce\Article.json"))
+            {
+                articles = await JsonSerializer.DeserializeAsync<List<Article>>(fs);
+            }
+            return articles;
+        }
+    }
+}
---
+++ Shared/Article.cs
@@ -0,0 +1,15 @@
+﻿namespace Academia.Shared
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public int Progress { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public string Pdf { get; set; }
+        public string Image { get; set; }
+        public int Year { get; set; }
+        public string Publisher { get; set; }
+        public string Author { get; set; }
+    }
+}
